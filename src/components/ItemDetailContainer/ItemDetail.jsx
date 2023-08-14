47a20d3afd0b273feef008be831cb0dfed1f8877
ecostabal <ecostabal@urbex.cl>
2023-08-14 16:38:16 -0400

    avanzando

--- src/components/ItemDetailContainer/ItemDetail.jsx
+++ src/components/ItemDetailContainer/ItemDetail.jsx
@@ -1,33 +1,50 @@
-import React from "react";
-import { Link } from 'react-router-dom'; // Importa Link desde react-router-dom
-import ItemCount from '../ItemCount/ItemCount';
+import { useState, useEffect, useContext } from "react";
+import { getProductData } from "../../services/asyncMock";
+import { Link, useParams } from "react-router-dom";
 import ButtonComponent from "../ButtonComponent/ButtonComponent";
+import ItemCount from "../ItemCount/ItemCount";
+import { cartContext } from "../../context/cartContext";
 
-function ItemDetail(props) {
-  const {product} = props;
-  const handleAddToCart = () => {
-    // Lógica para agregar al carrito
-  };
-    return (
-        <div className="item-detail-container"> {/* Aplica la clase CSS para el contenedor principal */}
-        <div>
-          <img className="item-detail-image" src={product.img} alt="" /> {/* Aplica la clase CSS para la imagen */}
-        </div>
-        <div>
-          <h2 className="item-detail-title">{product.title}</h2> {/* Aplica la clase CSS para el título */}
-        </div>
-        <div>
-          <h4 className="item-detail-price">{product.price}</h4> {/* Aplica la clase CSS para el precio */}
-          <small className="item-detail-description">{product.description}</small> {/* Aplica la clase CSS para la descripción */}
-        
-        </div>
-        <ItemCount stock={4} onConfirm={handleAddToCart} />
-        <Link to="/">
-        <ButtonComponent>Volver al inicio</ButtonComponent>
-        </Link>
-        <button class="item-detail-add-to-cart">Agregar</button>
-      </div>
-    )
+function ItemDetailContainer() {
+  const [product, setProduct] = useState({});
+  const { id } = useParams();
+
+  const { addToCart } = useContext(cartContext);
+
+  useEffect(() => {
+    async function requestProduct() {
+      const respuesta = await getProductData(id);
+      setProduct(respuesta);
+    }
+
+    requestProduct();
+  }, [id]);
+
+  function handleAddToCart(clickCount) {
+    addToCart(product, clickCount);
+    alert(`Producto agregado al carrito, cantidad: ${clickCount}`);
+  }
+
+  return (
+    <div className="item-detail-container"> {/* Aplica la clase CSS para el contenedor principal */}
+    <div>
+      <img className="item-detail-image" src={product.img} alt="" /> {/* Aplica la clase CSS para la imagen */}
+    </div>
+    <div>
+      <h2 className="item-detail-title">{product.title}</h2> {/* Aplica la clase CSS para el título */}
+    </div>
+    <div>
+      <h4 className="item-detail-price">{product.price}</h4> {/* Aplica la clase CSS para el precio */}
+      <small className="item-detail-description">{product.description}</small> {/* Aplica la clase CSS para la descripción */}
+    
+    </div>
+    <ItemCount stock={4} onConfirm={handleAddToCart} />
+    <Link to="/">
+    <ButtonComponent>Volver al inicio</ButtonComponent>
+    </Link>
+    <button class="item-detail-add-to-cart">Agregar</button>
+  </div>
+  );
 }
 
-export default ItemDetail
+export default ItemDetailContainer;
